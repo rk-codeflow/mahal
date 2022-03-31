d8a79fe840159d1786034547f1575338448235eb
Raj Chaudhary <rajkiran@view9.com.np>
2022-03-31 09:38:05 +0545

    pages linked

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,10 +1,10 @@
     //slick slider 
-    // $(document).ready(function(){
-    //     $('.ins-carousel').slick({
-    //         dots: true,
-    //         nextArrow: '<button type="button" class="next"><i class="fas fa-angle-right fa-lg"></i></button>',
-    //     });
-    // });
+    $(document).ready(function(){
+        $('.ins-carousel').slick({
+            dots: true,
+            nextArrow: '<button type="button" class="next"><i class="fas fa-angle-right fa-lg"></i></button>',
+        });
+    });
 
     
 
